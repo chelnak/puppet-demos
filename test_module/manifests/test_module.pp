726a5309cc3319eb1e3129511f7f74ca7dedcbf0
Craig Gumbley <craiggumbley@gmail.com>
2022-05-18 11:46:24 +0100

    Adding a new puppet class

---
+++ test_module/manifests/test_module.pp
@@ -0,0 +1,12 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include wakka
+# 
+class test_module::test_module {
+  package { 'wget':
+    ensure => installed,
+  // }
+}
