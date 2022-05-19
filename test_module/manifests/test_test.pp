45b65ce0b8d2e3eb30778845ee64d75354311bf9
Craig Gumbley <craiggumbley@gmail.com>
2022-05-19 12:01:21 +0100

    A new puppet module that needs some validation

---
+++ test_module/manifests/test_test.pp
@@ -0,0 +1,11 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include wakka
+#
+class test_module::test_test {
+  package { 'wget':
+    ensure => installed,
+}
